1aa8535fdff5a78dc7962bf2bd115652d04ddaa6
rexshihaoren <renshihao@gmail.com>
2013-11-25 12:02:47 -0800

    activerecord validation added

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,22 +10,30 @@ class MoviesController < ApplicationController
   	@movie=Movie.find(id)
   end
   def new
+    @movie=Movie.new # convention over configuration, this line is not actually needed
   end
   def create
-  	@movie=Movie.create!(params[:movie])
-  	flash[:notice]="#{@movie.title} was successfully created."
-	redirect_to movies_path
+  	@movie=Movie.new(params[:movie])
+    if @movie.save
+  	 flash[:notice]="#{@movie.title} was successfully created."
+	   redirect_to movies_path
+    else
+      render 'new'
+    end
   end
   def edit
   	@movie=Movie.find params[:id]
   end
   def update
   	@movie=Movie.find params[:id]
-  	@movie.update_attributes!(params[:movie])
-  	flash[:notice] = "#{@movie.title} was successfully updated"
-  	respond_to do |client_wants|
-  		client_wants.html {redirect_to movie_path(@movie)}
-  		client_wants.xml {render :xml => movie.to_xml}
+  	if @movie.update_attributes(params[:movie])
+    	flash[:notice] = "#{@movie.title} was successfully updated"
+    	respond_to do |client_wants|
+    		client_wants.html {redirect_to movie_path(@movie)}
+    		client_wants.xml {render :xml => movie.to_xml}
+      end
+      else
+        render 'edit'
   	end
   end
   def destroy
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,15 @@ class Movie < ActiveRecord::Base
 	def self.all_ratings
     	%w(G PG PG-13 NC-17 R)
   	end
+  	validates :title, :presence => true
+  	validates :release_date, :presence =>true
+  	validate :released_1930_or_later # validate other than validates for customed validations
+  	validates :rating, :inclusion => {:in => Movie.all_ratings},
+  		:unless => :grandfathered?
+  	def released_1930_or_later
+  		errors.add(:release_date , 'must be 1930 or later') if
+  			self.release_date < Date.parse('1 Jan 1930')
+  	end
+  	@@grandfathered_date = Date.parse('1 Nov 1968')
+  	def grandfathered? ; self.release_date >= @@grandfathered_date ; end
 end
